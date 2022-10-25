1046a919bf7ff013471d68f694ef550b52da4010
brnysn <brnysn@users.noreply.github.com>
2022-10-25 19:59:08 +0000

    Fix styling

--- src/Database/Seeders/WorldSeeder.php
+++ src/Database/Seeders/WorldSeeder.php
@@ -18,30 +18,30 @@ class WorldSeeder extends Seeder
 
     protected function seedCountries()
     {
-        $csvFile = fopen(__DIR__.'/countries.sql', "r");
+        $csvFile = fopen(__DIR__.'/countries.sql', 'r');
         $firstline = true;
-        while (($data = fgetcsv($csvFile, 2000, ",")) !== false) {
+        while (($data = fgetcsv($csvFile, 2000, ',')) !== false) {
             if (! $firstline) {
                 Country::insert([
-                    'id' => $data[ 0 ],
-                    'name' => $data[ 1 ],
-                    'iso3' => $data[ 2 ],
-                    'iso2' => $data[ 3 ],
-                    'numeric_code' => $data[ 4 ],
-                    'phone_code' => $data[ 5 ],
-                    'capital' => $data[ 6 ],
-                    'currency' => $data[ 7 ],
-                    'currency_name' => $data[ 8 ],
-                    'currency_symbol' => $data[ 9 ],
-                    'tld' => $data[ 10 ],
-                    'native' => $data[ 11 ],
-                    'region' => $data[ 12 ],
-                    'subregion' => $data[ 13 ],
-                    'timezones' => $data[ 14 ],
-                    'latitude' => $data[ 15 ],
-                    'longitude' => $data[ 16 ],
-                    'emoji' => $data[ 17 ],
-                    'emojiU' => $data[ 18 ],
+                    'id' => $data[0],
+                    'name' => $data[1],
+                    'iso3' => $data[2],
+                    'iso2' => $data[3],
+                    'numeric_code' => $data[4],
+                    'phone_code' => $data[5],
+                    'capital' => $data[6],
+                    'currency' => $data[7],
+                    'currency_name' => $data[8],
+                    'currency_symbol' => $data[9],
+                    'tld' => $data[10],
+                    'native' => $data[11],
+                    'region' => $data[12],
+                    'subregion' => $data[13],
+                    'timezones' => $data[14],
+                    'latitude' => $data[15],
+                    'longitude' => $data[16],
+                    'emoji' => $data[17],
+                    'emojiU' => $data[18],
                 ]);
             }
             $firstline = false;
@@ -51,21 +51,21 @@ class WorldSeeder extends Seeder
 
     protected function seedStates()
     {
-        $csvFile = fopen(__DIR__.'/states.sql', "r");
+        $csvFile = fopen(__DIR__.'/states.sql', 'r');
 
         $firstline = true;
-        while (($data = fgetcsv($csvFile, 2000, ",")) !== false) {
+        while (($data = fgetcsv($csvFile, 2000, ',')) !== false) {
             if (! $firstline) {
                 State::insert([
-                    'id' => $data[ 0 ],
-                    'name' => $data[ 1 ],
-                    'country_id' => $data[ 2 ],
-                    'country_code' => $data[ 3 ],
-                    'country_name' => $data[ 4 ],
-                    'state_code' => $data[ 5 ],
-                    'type' => $data[ 6 ],
-                    'latitude' => $data[ 7 ],
-                    'longitude' => $data[ 8 ],
+                    'id' => $data[0],
+                    'name' => $data[1],
+                    'country_id' => $data[2],
+                    'country_code' => $data[3],
+                    'country_name' => $data[4],
+                    'state_code' => $data[5],
+                    'type' => $data[6],
+                    'latitude' => $data[7],
+                    'longitude' => $data[8],
                 ]);
             }
             $firstline = false;
@@ -75,22 +75,22 @@ class WorldSeeder extends Seeder
 
     protected function seedCities()
     {
-        $csvFile = fopen(__DIR__.'/cities.sql', "r");
+        $csvFile = fopen(__DIR__.'/cities.sql', 'r');
         $firstline = true;
-        while (($data = fgetcsv($csvFile, 2000, ",")) !== false) {
+        while (($data = fgetcsv($csvFile, 2000, ',')) !== false) {
             if (! $firstline) {
                 City::insert([
-                    'id' => $data[ 0 ],
-                    'name' => $data[ 1 ],
-                    'state_id' => $data[ 2 ],
-                    'state_code' => $data[ 3 ],
-                    'state_name' => $data[ 4 ],
-                    'country_id' => $data[ 5 ],
-                    'country_code' => $data[ 6 ],
-                    'country_name' => $data[ 7 ],
-                    'latitude' => $data[ 8 ],
-                    'longitude' => $data[ 9 ],
-                    'wikiDataId' => $data[ 10 ],
+                    'id' => $data[0],
+                    'name' => $data[1],
+                    'state_id' => $data[2],
+                    'state_code' => $data[3],
+                    'state_name' => $data[4],
+                    'country_id' => $data[5],
+                    'country_code' => $data[6],
+                    'country_name' => $data[7],
+                    'latitude' => $data[8],
+                    'longitude' => $data[9],
+                    'wikiDataId' => $data[10],
                 ]);
             }
             $firstline = false;
@@ -98,8 +98,3 @@ class WorldSeeder extends Seeder
         fclose($csvFile);
     }
 }
-
-
-
-
-
