acfc8cd9c52eb6ea9046d36be896789f06979a21
Yasin BARAN <brnysn@gmail.com>
2022-10-25 22:58:16 +0300

    update

--- src/Database/Seeders/WorldSeeder.php
+++ src/Database/Seeders/WorldSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Brnysn\World\Database\Seeders;
 
-use DB;
+use Brnysn\World\Models\City;
+use Brnysn\World\Models\Country;
+use Brnysn\World\Models\State;
 use Illuminate\Database\Seeder;
 
 class WorldSeeder extends Seeder
@@ -16,22 +18,88 @@ class WorldSeeder extends Seeder
 
     protected function seedCountries()
     {
-        $sql = file_get_contents(__DIR__.'/countries.sql');
-
-        DB::unprepared($sql);
+        $csvFile = fopen(__DIR__.'/countries.sql', "r");
+        $firstline = true;
+        while (($data = fgetcsv($csvFile, 2000, ",")) !== false) {
+            if (! $firstline) {
+                Country::insert([
+                    'id' => $data[ 0 ],
+                    'name' => $data[ 1 ],
+                    'iso3' => $data[ 2 ],
+                    'iso2' => $data[ 3 ],
+                    'numeric_code' => $data[ 4 ],
+                    'phone_code' => $data[ 5 ],
+                    'capital' => $data[ 6 ],
+                    'currency' => $data[ 7 ],
+                    'currency_name' => $data[ 8 ],
+                    'currency_symbol' => $data[ 9 ],
+                    'tld' => $data[ 10 ],
+                    'native' => $data[ 11 ],
+                    'region' => $data[ 12 ],
+                    'subregion' => $data[ 13 ],
+                    'timezones' => $data[ 14 ],
+                    'latitude' => $data[ 15 ],
+                    'longitude' => $data[ 16 ],
+                    'emoji' => $data[ 17 ],
+                    'emojiU' => $data[ 18 ],
+                ]);
+            }
+            $firstline = false;
+        }
+        fclose($csvFile);
     }
 
     protected function seedStates()
     {
-        $sql = file_get_contents(__DIR__.'/states.sql');
+        $csvFile = fopen(__DIR__.'/states.sql', "r");
 
-        DB::unprepared($sql);
+        $firstline = true;
+        while (($data = fgetcsv($csvFile, 2000, ",")) !== false) {
+            if (! $firstline) {
+                State::insert([
+                    'id' => $data[ 0 ],
+                    'name' => $data[ 1 ],
+                    'country_id' => $data[ 2 ],
+                    'country_code' => $data[ 3 ],
+                    'country_name' => $data[ 4 ],
+                    'state_code' => $data[ 5 ],
+                    'type' => $data[ 6 ],
+                    'latitude' => $data[ 7 ],
+                    'longitude' => $data[ 8 ],
+                ]);
+            }
+            $firstline = false;
+        }
+        fclose($csvFile);
     }
 
     protected function seedCities()
     {
-        $sql = file_get_contents(__DIR__.'/cities.sql');
-
-        DB::unprepared($sql);
+        $csvFile = fopen(__DIR__.'/cities.sql', "r");
+        $firstline = true;
+        while (($data = fgetcsv($csvFile, 2000, ",")) !== false) {
+            if (! $firstline) {
+                City::insert([
+                    'id' => $data[ 0 ],
+                    'name' => $data[ 1 ],
+                    'state_id' => $data[ 2 ],
+                    'state_code' => $data[ 3 ],
+                    'state_name' => $data[ 4 ],
+                    'country_id' => $data[ 5 ],
+                    'country_code' => $data[ 6 ],
+                    'country_name' => $data[ 7 ],
+                    'latitude' => $data[ 8 ],
+                    'longitude' => $data[ 9 ],
+                    'wikiDataId' => $data[ 10 ],
+                ]);
+            }
+            $firstline = false;
+        }
+        fclose($csvFile);
     }
 }
+
+
+
+
+
